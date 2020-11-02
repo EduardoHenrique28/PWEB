481e9882427dc0a4b914ea94439a061d7479aab2
EduardoHenrique28 <69875958+EduardoHenrique28@users.noreply.github.com>
2020-11-02 10:28:33 -0300

    Add files via upload

---
+++ Atividade10/js/script.js
@@ -0,0 +1,14 @@
+var janela = document.getElementById("janela");
+
+
+function abrirJanela(){
+	janela.src = "img/janela_aberta.jpg";
+}
+
+function fecharJanela(){
+	janela.src = "img/janela_Fechada.jpg";
+}
+
+function quebrarJanela(){
+	janela.src = "img/janela_quebrada.jpg";
+}
